3eef163c654a31f954e6be6978c2fdb11e657934
hana <hana1155@hanmail.net>
2025-08-01 15:50:48 +0900

    MainSlide.vue 구현 (Swiper)

--- src/components/MainSlide.vue
+++ src/components/MainSlide.vue
@@ -1,17 +1,87 @@
 <template>
-    <div class="main-slide">
-        <div class="slide-banner">슬라이드</div>
-    </div>
+  <div class="main-slide">
+    <swiper :options="swiperOption" class="swiper-container">
+      <swiper-slide v-for="(item, i) in slides" :key="i">
+        <img :src="item.img" :alt="item.alt" class="slide-img" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
+  </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  name: "MainSlide",
+  data() {
+    return {
+      slides: [
+        { img: "https://img.megagong.net/m/2025/0724_pass/gong/pass_gong.png", alt: "배너1" },
+        { img: "https://img.megagong.net/m/2025/0724_pass/tech/pass_tech.png", alt: "배너2" },
+        { img: "https://img.megagong.net/m/2025/0724_pass/army/pass_army.png", alt: "배너3" },
+      ],
+      swiperOption: {
+        loop: true,
+        autoplay: {
+          delay: 3000,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+    };
+  },
+};
+</script>
+<style>
 .main-slide {
-  background: #eaf3ff;
-  height: 300px;
+  width: 790px;
+  margin: auto;
+  position: relative;
+}
+.slide-img {
+  width: 100%;
+  display: block;
+}
+.swiper-button-prev,
+.swiper-button-next {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 10;
   display: flex;
   align-items: center;
   justify-content: center;
-  border-radius: 12px;
-  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.05);
+  cursor: pointer;
+}
+.swiper-button-prev {
+  left: 10px;
+}
+.swiper-button-next {
+  right: 10px;
+}
+.swiper-button-prev::after,
+.swiper-button-next::after {
+  font-size: 20px;
+  font-weight: bold;
+}
+.swiper-button-prev::after {
+  content: "‹";
+}
+.swiper-button-next::after {
+  content: "›";
+}
+.swiper-pagination {
+  bottom: 10px !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/css/swiper.css"; //swiper 5용 css
+
 import "./assets/base.scss";
+
+Vue.use(VueAwesomeSwiper);
 
 Vue.config.productionTip = false;
 
